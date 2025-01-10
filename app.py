94308c9cf673bb6fa476c8be74c96143b4d58db7
online-shop <atefepournasrolah@gmail.com>
2025-01-10 23:07:49 +0330

    blueprint

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from blueprint.general import app as general
 
 app = Flask(__name__)
-
+app.register_blueprint(general)
 
 @app.route('/')
 def hello_world():  # put application's code here
