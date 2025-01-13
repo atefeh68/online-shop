8c0d06f8843118fa3898462dfbf61260a25882f7
online-shop <atefepournasrolah@gmail.com>
2025-01-13 12:29:13 +0330

    create database

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from blueprint.general import app as general
-
+from flask_sqlalchemy import SQLAlchemy
+import config
 app = Flask(__name__)
 app.register_blueprint(general)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == '__main__':
