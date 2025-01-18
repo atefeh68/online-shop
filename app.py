c15a33089704293b9b6ba7c61b1d6aeef61fbf55
online-shop <atefepournasrolah@gmail.com>
2025-01-18 09:41:09 +0330

    file extenions

--- app.py
+++ app.py
@@ -1,24 +1,19 @@
 from flask import Flask
 from blueprint.general import app as general
 from blueprint.admin import app as admin
-from flask_sqlalchemy import SQLAlchemy
+import extentions
 import config
 app = Flask(__name__)
 app.register_blueprint(general)
 app.register_blueprint(admin)
 
-from sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeBase):
-  pass
-
-db = SQLAlchemy(model_class=Base)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
-db.init_app(app)
+extentions.db.init_app(app)
 
 with app.app_context():
-    db.create_all()
+    extentions.db.create_all()
 
 
 if __name__ == '__main__':
--- models/user.py
+++ models/user.py
@@ -1,5 +1,7 @@
-class User(Base):
-    __tablename__ = 'users'
+from sqlalchemy import *
+from extentions import db
+
+class User(db.models):
     id = Column(Integer, primary_key=True)
     name = Column(String(50), unique=True)
     email = Column(String(120), unique=True)
