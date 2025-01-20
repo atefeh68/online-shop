15bd9abe831ddc63b3842d32caa2464202602144
online-shop <atefepournasrolah@gmail.com>
2025-01-20 11:53:58 +0330

    file admin of blueprint

--- blueprint/admin.py
+++ blueprint/admin.py
@@ -2,6 +2,13 @@ from flask import Blueprint, render_template, request, redirect, session, abort
 import config
 
 app = Blueprint('admin', __name__)
+
+@app.before_request
+def before_request():
+
+    if  session.get('admin_login', None) is None and request.endpoint != 'admin/login' :
+        abort(403)
+
 @app.route('/admin/login',methods=["POST", "GET"])
 def login():
     if request.method == "POST":
@@ -22,3 +29,9 @@ def dashboard():
     if session.get('admin_login', None) is None:
         abort(403)
     return "dashboard"
+
+@app.route('/admin/dashboard/product', methods=["GET"])
+def product():
+    if session.get('admin_login', None) is None:
+        abort(403)
+    return "product"
