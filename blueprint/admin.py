32ad71cfdabf58e1a474b6574d603710fc146b54
online-shop <atefepournasrolah@gmail.com>
2025-01-20 10:45:24 +0330

    admin login and dashboard

--- blueprint/admin.py
+++ blueprint/admin.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, session, abort
 import config
 
 app = Blueprint('admin', __name__)
@@ -9,9 +9,16 @@ def login():
         password = request.form.get('password', None)
 
         if username == config.ADMIN_USERNAME and password == config.ADMIN_PASSWORD:
+            session['admin_login'] = username
             return redirect("/admin/dashboard")
         else:
             return redirect("/admin/login")
 
     else:
         return render_template("admin/login.html")
+
+@app.route('/admin/dashboard', methods=["GET"])
+def dashboard():
+    if session.get('admin_login', None) is None:
+        abort(403)
+    return "dashboard"
--- config.py
+++ config.py
@@ -2,3 +2,4 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
 
 ADMIN_USERNAME = "admin"
 ADMIN_PASSWORD = "123"
+SECRET_KEY = 'gfughjlhb122kjgyuhoggregrknlhgj225'
